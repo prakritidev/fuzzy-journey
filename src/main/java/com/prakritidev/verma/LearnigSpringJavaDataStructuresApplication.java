fb7a52a8770e52c240cb1c69659d8d66b1aa7c51
prakritidev <prakritidevverma@gmail.com>
2022-12-02 21:16:05 +0530

    learning init

---
+++ src/main/java/com/prakritidev/verma/LearnigSpringJavaDataStructuresApplication.java
@@ -0,0 +1,13 @@
+package com.prakritidev.verma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnigSpringJavaDataStructuresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnigSpringJavaDataStructuresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prakritidev/verma/LearnigSpringJavaDataStructuresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prakritidev.verma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnigSpringJavaDataStructuresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
