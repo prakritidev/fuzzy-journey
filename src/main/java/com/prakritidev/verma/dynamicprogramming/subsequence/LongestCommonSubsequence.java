e2c9bc264bb596f2611010d7441b07a7741da624
prakritidev <prakritidevverma@gmail.com>
2022-12-29 23:35:11 +0530

    feat(dynamicProgramming): added lcsNonRecursive

--- src/main/java/com/prakritidev/verma/dynamicprogramming/subsequence/LongestCommonSubsequence.java
+++ src/main/java/com/prakritidev/verma/dynamicprogramming/subsequence/LongestCommonSubsequence.java
@@ -19,14 +19,35 @@ public class LongestCommonSubsequence {
 
         System.out.println("Length of LCS is" + " " +
                 lcs.lcs(X, Y, m, n, dp));
+        System.out.println("Length of LCS is" + " " +
+                lcs.lcsNonRecursive(X, Y, m, n));
         
     }
 
-    private int lcs(char[] x, char[] y, int m, int n, int[][]dp) {
+    private int lcsNonRecursive(char[] x, char[] y, int m, int n) {
+
+        int[][] cache = new int[m + 1][n + 1];
+        
+        for (int i = 0; i <=m; i++) {
+            for (int j = 0; j <=n; j++) {
+                if (i == 0 || j == 0)
+                    cache[i][j] = 0;
+                else if (x[i-1] == y[j-1]) {
+                    cache[i][j] = 1 + cache[i - 1][j - 1];
+                } else {
+                    cache[i][j] = Math.max(cache[i - 1][j], cache[i][j - 1]);
+                }
+            }
+        }
+        return cache[m][n];
+    }
+
+    private int lcs(char[] x, char[] y, int m, int n, int[][] dp) {
         if (m == 0 || n == 0) {
             return 0;
         }
-        if(dp[m][n] != -1) return dp[m][n];
+        if (dp[m][n] != -1)
+            return dp[m][n];
 
         if (x[m - 1] == y[n - 1])
             return dp[m][n] = 1 + lcs(x, y, m - 1, n - 1, dp);
@@ -34,5 +55,7 @@ public class LongestCommonSubsequence {
 
             return dp[m][n] = Math.max(lcs(x, y, m - 1, n, dp), lcs(x, y, m, n - 1, dp));
     }
+    
+
 
 }
